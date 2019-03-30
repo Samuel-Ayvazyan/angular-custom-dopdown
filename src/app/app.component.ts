4bd7cdaf2b2761c9ec08347e9408b84bda94f651
Samuel Ayvazyan <samuel.ayvazyan.fd@gmail.com>
2019-03-31 01:15:09 +0400

    Add few options for sa-angular-component for the demo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-custom-dropdown';
+  public arrOptions1 = [
+    { value: 10, text: 'Value10'},
+    { value: 20, text: 'Value20'},
+    { value: 30, text: 'Value30'},
+    { value: 40, text: 'Value40'},
+    { value: 50, text: 'Value50'},
+  ]
+  public arrOptions2 = [
+    { value: 100, text: 'Value100'},
+    { value: 200, text: 'Value200'},
+    { value: 300, text: 'Value300'},
+    { value: 400, text: 'Value400'},
+    { value: 500, text: 'Value500'},
+    { value: 600, text: 'Value600'},
+    { value: 700, text: 'Value700'},
+    { value: 800, text: 'Value800'},
+    { value: 900, text: 'Value900'},
+  ]
+  public arrOptions3 = [
+    { value: 1, text: 'Value1'},
+    { value: 2, text: 'Value2'},
+    { value: 3, text: 'Value3'},
+  ]
 }
